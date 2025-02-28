851e60a5c0bbd9199751798ba0272eae64fd849c
Vinisantas <viniciuspereirasantana@gmail.com>
2025-02-28 11:51:30 -0300

    Update routs and inserts on the table equpamentos

--- app/controller/routes.py
+++ app/controller/routes.py
@@ -12,53 +12,49 @@ from app.model.models import cadastrar_equipamentos
 
 @app.route('/', methods=['GET'])
 def get_equipamentos():
-        status_banco = verificar_banco()
-        if status_banco ["status"] == "sucesso":
-                conn = sqlite3.connect("cadastro_patrimonio.sqlite")
-                cursor = conn.cursor()
-
-                #listar todos os equipamentos
-                lista = cursor.execute(''' SELECT * FROM  equipamentos ''').fetchall()
-
-                #fechamento da conexão
-                conn.close()
-
-                return make_response(
-                        jsonify(mensagem=f"equipamentos: {lista}"),200
-                )
-        else:
-                return make_response(
-                        jsonify(mensagem="Erro ao conectar ao banco de dados."),500
-                )
+                status_banco = verificar_banco()
+                if status_banco ["status"] == "sucesso":
+                        conn = sqlite3.connect("data/cadastro_patrimonio.sqlite")
+                        cursor = conn.cursor()
+                        #listar todos os equipamentos
+                        lista = cursor.execute(''' SELECT * FROM  equipamentos ''').fetchall()
+                        #fechamento da conexão
+                        conn.close()
+                        return make_response(
+                                jsonify(lista),200
+                        )
+                else:
+                        return make_response(
+                                jsonify(mensagem="Erro ao conectar ao banco de dados."),500
+                        )
 
 
 
 
 @app.route('/patrimonio/<string:patrimonio>', methods=['GET'])
-def get_patrimonio():
+def get_patrimonio(patrimonio):
         status_banco = verificar_banco()
         if status_banco ["status"] == "sucesso":
-                conn = sqlite3.connect("cadastro_patrimonio.sqlite")
-                cursor = conn.cursor()
+                try:
+                        conn = sqlite3.connect("data/cadastro_patrimonio.sqlite")
+                        cursor = conn.cursor()
 
-                #listar equipamentos por patrimônio
-                cursor.execute(''' SELECT *  FROM equipamentos WHERE patrimonio = ? ''').fetchall()
-                equipamento_patrimonio = cursor.fetchall()
-
-                if not equipamento_patrimonio:
+                        #listar equipamentos por patrimônio
+                        equipamento_patrimonio = cursor.execute(''' SELECT *  FROM equipamentos WHERE patrimonio = ? ''', (patrimonio,)).fetchall()
+                        if not equipamento_patrimonio:
+                                return make_response(
+                                        jsonify(mensagem=f"Patrimônio não encontrado"),404
+                                )
+                        if equipamento_patrimonio:
+                                return make_response(
+                                        jsonify(equipamento=equipamento_patrimonio),200
+                                )
+                finally:
                         conn.close()
-                        return make_response(
-                                jsonify(mensagem=f"Patrimônio {equipamento_patrimonio} não encontrado"),201
-                )
-
-                if equipamento_patrimonio:
-                        return make_response(
-                                jsonify(f"equipamento: {equipamento_patrimonio}"),200
-                        )
         else:
                 return make_response(
                         jsonify(mensagem="Erro ao conectar ao banco de dados."),500
-                )
+                                )
 
 
 
@@ -70,13 +66,17 @@ def post_equipamentos():
                 return make_response(
                 jsonify(mensagem="Dados inválidos ou incompletos."), 400
                 )
+        if not dados["patrimonio"] or not dados["nome"] or not dados["descricao"] or not dados["localizacao"] or not dados["data_de_compra"]:
+                return make_response(
+                        jsonify(mensagem="Nenhum campo pode estar vazio!"), 400
+                )
         status_banco = verificar_banco()
         if status_banco["status"] == "sucesso":
                 resultado = cadastrar_equipamentos(dados)
                 if resultado["status"] == "sucesso":
-                        return make_response(jsonify(mensagem=resultado["mensagem"]), 201)
+                        return make_response(jsonify(mensagem=resultado["mensagem"]), 200)
                 else:
-                        return make_response(jsonify(mensagem=resultado["mensagem"]), 500)
+                        return make_response(jsonify(mensagem=resultado["mensagem"]), 409)
         else:
                 return make_response(
                 jsonify(mensagem="Erro ao conectar ao banco de dados."), 500
@@ -91,24 +91,22 @@ def post_equipamentos():
 def delete_equipamentos(patrimonio):
         status_banco = verificar_banco()
         if status_banco ["status"] == "sucesso":
-                conn = sqlite3.connect("cadastro_patrimonio.sqlite")
+                conn = sqlite3.connect("data/cadastro_patrimonio.sqlite")
                 cursor = conn.cursor() 
 
                 cursor.execute('''SELECT * FROM equipamentos WHERE patrimonio = ?''', (patrimonio,))
                 equipamento = cursor.fetchall()
-
                 if not equipamento:
                         conn.close()
                         return make_response(
                                 jsonify(mensagem=f"Patrimônio {patrimonio} não encontrado"),404
                 )
-
                 if equipamento:
                         cursor.execute(''' DELETE FROM equipamentos WHERE patrimonio = ?''', (patrimonio,))
                         conn.commit()
                         conn.close()
                         return make_response(
-                                jsonify(mensagem=f"Patrimônio {patrimonio} deletado com sucesso!"),204
+                                jsonify(mensagem=f"Patrimônio {patrimonio} deletado com sucesso!"),200
                 )
         else:
                 return make_response(
--- app/model/models.py
+++ app/model/models.py
@@ -4,15 +4,20 @@ from app.model.database import verificar_banco
 # Inserção de dados na tabela equipamentos
 def cadastrar_equipamentos(dados):
         try:    
-                conn = sqlite3.connect("cadastro_patrimonio.sqlite")
+                conn = sqlite3.connect("data/cadastro_patrimonio.sqlite")
                 cursor = conn.cursor()
-                cursor.execute('''
-                        INSERT INTO equipamentos ( patrimonio, nome, descricao, localizacao, data_de_compra)
-                        VALUES (?, ?, ?, ?, ?)
-                        ''', (dados["patrimonio"], dados["nome"], dados["descricao"], dados["localizacao"], dados["data_de_compra"]))
-                conn.commit()
-                conn.close()
-                return {"status": "sucesso", "mensagem": "Equipamento cadastrado com sucesso!"}
+                verifica_existencia = cursor.execute(''' SELECT * FROM equipamentos WHERE patrimonio = ? ''', (dados["patrimonio"],)).fetchone()
+                if verifica_existencia:
+                        return {"status": "erro", "mensagem": "Equipamento já está cadastrado!"}
+                else:
+                        cursor.execute('''
+                                INSERT INTO equipamentos ( patrimonio, nome, descricao, localizacao, data_de_compra)
+                                VALUES (?, ?, ?, ?, ?)
+                                ''', (dados["patrimonio"], dados["nome"], dados["descricao"], dados["localizacao"], dados["data_de_compra"]))
+                
+                        conn.commit()
+                        conn.close()
+                        return {"status": "sucesso", "mensagem": "Equipamento cadastrado com sucesso!"}
         
         except sqlite3.Error as e:
 
